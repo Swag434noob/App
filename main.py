8434d19cc53343f1e288b996035997d610f344d1
Swag434noob <najibadam145@gmail.com>
2024-10-07 08:57:36 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello():
     return "Hello, Azure!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-db = mysql.connector.connect(
-    host="mydbserver.mysql.database.azure.com",
-    user="adminuser@mydbserver",
-    password="MyPassword!",
-    database="mydatabase"
-)
-
-
-import mysql.connector
+    app.run(host='0.0.0.0', port=8080)
